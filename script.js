fef6c0f12138cfebd23987902f82e7d077cd58e0
Vald <35408460+gobllin@users.noreply.github.com>
2023-11-16 21:36:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+ const image = document.querySelector('img');
+
+ image.addEventListener('click', function () {
+     // Change the image source on click
+     image.src = 'e858678426357728038c277598871d6d.gif'; // Replace with the path to another image
+ });
+});
